969f103079ee0cd1c035ef34079b801d76a9060e
anton <antonguy@ukr.net>
2021-02-14 21:03:38 +0200

    lesson 13

--- app/src/main/java/com/example/cloneinstagram/activities/LoginActivity.kt
+++ app/src/main/java/com/example/cloneinstagram/activities/LoginActivity.kt
@@ -30,6 +30,7 @@ class LoginActivity : AppCompatActivity(), KeyboardVisibilityEventListener, Text
         login_email_input.addTextChangedListener(this)
         login_password_input.addTextChangedListener(this)
         login_btn.setOnClickListener(this)
+        create_account_text.setOnClickListener(this)
         mAuth = FirebaseAuth.getInstance()
     }
 
@@ -58,17 +59,25 @@ class LoginActivity : AppCompatActivity(), KeyboardVisibilityEventListener, Text
     }
 
     override fun onClick(view: View) {
-        val email = login_email_input.text.toString()
-        val password = login_password_input.text.toString()
-        if (validate(email, password)) {
-            mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener {
-                    startActivity(Intent(this, HomeActivity::class.java))
-                    finish()
+        when(view.id){
+            R.id.login_btn -> {
+                val email = login_email_input.text.toString()
+                val password = login_password_input.text.toString()
+                if (validate(email, password)) {
+                    mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener {
+                            startActivity(Intent(this, HomeActivity::class.java))
+                            finish()
+                        }
+                } else {
+                    showToast("Please enter email and password")
                 }
-        } else {
-            showToast("Please enter email and password")
+            }
+            R.id.create_account_text -> {
+                startActivity(Intent(this,RegisterActivity::class.java))
+            }
         }
+
     }
 
     private fun validate(email: String, password: String) =
--- app/src/main/java/com/example/cloneinstagram/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/cloneinstagram/activities/RegisterActivity.kt
@@ -1,24 +1,30 @@
 package com.example.cloneinstagram.activities
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.cloneinstagram.R
+import com.example.cloneinstagram.models.User
 import com.example.cloneinstagram.showToast
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.fragment_register_email.*
 import kotlinx.android.synthetic.main.fragment_register_namepass.*
+import java.util.*
 
 class RegisterActivity : AppCompatActivity(), EmailFragment.Listener, NamePassFragment.Listener {
+    private val TAG = "RegisterActivity"
     private var mEmail: String? = null
-    private lateinit var mAuth:FirebaseAuth
-    private lateinit var mDatabase:DatabaseReference
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDatabase: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,22 +38,65 @@ class RegisterActivity : AppCompatActivity(), EmailFragment.Listener, NamePassFr
     }
 
     override fun onNext(email: String) {
-        if (email.isNotEmpty()){
+        if (email.isNotEmpty()) {
             mEmail = email
-            supportFragmentManager.beginTransaction().replace(R.id.frame_layout,NamePassFragment())
+            // don`t replace to new NamePassFragment
+            supportFragmentManager.beginTransaction().replace(R.id.frame_layout, NamePassFragment())
                 .addToBackStack(null)
                 .commit()
-        } else{
+            println("next")
+        } else {
             showToast("Please enter email")
         }
     }
 
     override fun onRegister(fullname: String, password: String) {
-        if (fullname.isNotEmpty()&&password.isNotEmpty()){
-
-        }else{
+        if (fullname.isNotEmpty() && password.isNotEmpty()) {
+            val email = mEmail
+            if (email != null) {
+                mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val user = makeUser(fullname, email)
+                            val reference = mDatabase.child("users").child(it.result!!.user!!.uid)
+                            reference.setValue(user)
+                                .addOnCompleteListener {
+                                    if (it.isSuccessful) {
+                                        startHomeActivity()
+                                    } else {
+                                        unknowRegistererror(it)
+                                    }
+                                }
+                        } else {
+                            unknowRegistererror(it)
+                        }
+                    }
+            } else {
+                Log.e(TAG, "onRegister: email is null")
+                showToast("please enter email")
+                supportFragmentManager.popBackStack()
+            }
+        } else {
             showToast("Please enter fullname and password")
         }
+    }
+
+    private fun unknowRegistererror(it: Task<out Any>) {
+        Log.e(TAG, "failed to create user profile", it.exception)
+        showToast("Something wrong happened. Please try again")
+    }
+
+    private fun startHomeActivity() {
+        startActivity(Intent(this, HomeActivity::class.java))
+        finish()
+    }
+
+    private fun makeUserName(fullname: String) =
+        fullname.toLowerCase(Locale.getDefault()).replace(" ", ".")
+
+    private fun makeUser(fullname: String, email: String): User {
+        val username = makeUserName(fullname)
+        return User(name = fullname, username = username, email = email)
     }
 }
 
@@ -57,6 +106,7 @@ class EmailFragment : Fragment() {
     interface Listener {
         fun onNext(email: String)
     }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -84,6 +134,7 @@ class NamePassFragment : Fragment() {
     interface Listener {
         fun onRegister(fullname: String, password: String)
     }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -91,11 +142,12 @@ class NamePassFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_register_namepass, container, false)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         next_btn.setOnClickListener {
             val fullname = register_fullname_input.text.toString()
             val password = register_password_input.text.toString()
-            mListener.onRegister(fullname,password)
+            mListener.onRegister(fullname, password)
         }
     }
 
